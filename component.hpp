8284b4e7b768cf59363c48a541ae6e85a7f19f3d
tjm1518 <54404449+tjm1518@users.noreply.github.com>
2020-06-02 00:39:05 +0100

    I think component's done?

--- component.hpp
+++ component.hpp
@@ -2,12 +2,14 @@
 #define component_hpp
 
 #include <string>
+#include <functional>
 
 using namespace std;
 
 class Node{ //A node. As the nodes are numbered 0 or from N001 to N999 we can give them a unique integer ID directly from the CIR file
 public:
 	int ID; //Used as the key for the right and left component maps
+	double voltage;
 	Node(int id);
 	Node();
 };
@@ -18,7 +20,7 @@ Node::Node(){ //Constructorfor either an empty node or the reference node
 	this->ID=0;
 }
 
-class Sim{ //Currently unused struct for storing the type of simulations. Potentially worth merging with SimParams. Structs DC and Tran inherit from this.
+class Sim{ //Currently unused struct for toring the type of simulations. Potentially worth merging with SimParams. Structs DC and Tran inherit from this.
 public:
     string simType;
 };
@@ -54,29 +56,34 @@ public:
     Node* right; //The node to the "right" of this component. This is the cathode/positive pin of polar components.
     Node* left; //The node to the "left" of this component. This is the anode/negative pin of polar components.
 };
-class LinComponent:Component{ //A linear component such as a resistor, capacitor, inductor or non-dependant source
+class Resistor:Component{ //A linear component such as a resistor, capacitor, inductor or non-dependant source
 public:
 	double val; //the value of the component in SI units. In sources this is the DC offset.
-    LinComponent(string cName, string uName, int id, Node& right, Node& left, double val);
-    LinComponent Resistor(string uName, int id, Node& right, Node& left, double val){
-        return LinComponent("Resistor", uName, id, right, left, val);
-    }
-    LinComponent Capacitor(string uName, int id, Node& right, Node& left, double val){
-        return LinComponent("Resistor", uName, id, right, left, val);
-    }
-    LinComponent Inductor(string uName, int id, Node& right, Node& left, double val){
-        return LinComponent("Resistor", uName, id, right, left, val);
-    }
+    Resistor(string uName, int id, Node& right, Node& left, double val);
 };
-LinComponent::LinComponent(string cName, string uName, int id, Node& right, Node& left, double val){
-    this->cName = cName;
+Resistor::Resistor(string uName, int id, Node& right, Node& left, double val){
+    this->cName = "Resistor";
     this->uName = uName;
     this->id = id;
     this->right = &right;
     this->left = &left;
     this->val = val;
 }
-
+class Source:Component{ //Only voltage sources here, I heard that current kills
+public:
+	double DCOffset; //For use in case of DC bias point check only. 500€ fine for misuse.
+	function<double(double)> waveform; //Functional programmers represent
+	Source(function<double(double)> f, double offset);
+	Source(double offset);
+};
+Source::Source(function<double(double)> f, double offset){
+	this->DCOffset = offset;
+	this->waveform = f;
+}
+Source::Source(double offset){
+	this->DCOffset = offset;
+	this->waveform = [](double d) {return offset;};
+}
 
 
 #endif
