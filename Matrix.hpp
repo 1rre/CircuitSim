6e495f5650577c0dd4f9583e99c572e821802a30
sc9318 <sc9318@ic.ac.uk>
2020-06-02 05:40:28 +0100

    file

--- Matrix.hpp
+++ Matrix.hpp
@@ -1,1 +1,21 @@
-gedbg
+
+#include <iostream>
+#include <armadillo>
+#include <cassert>
+#include <cmath>
+#include "component.hpp"
+
+using namespace std;
+using namespace arma;
+
+//Mat<double> GetNext_A(Mat<double A, Mat<double> X, Mat<double> Y, vector<LinComponent> source, vector<LinComponent> resistor, double time,)
+
+Mat<double> SolveX(Mat<double> A, Mat<double> Z)
+{
+  Mat<double> A_in = inv(A);//inverting the A matrix
+  cout<<A_in<<endl;
+  Mat<double> X_n = A_in*Z; //multiplying the inverse by Z to get X
+  return X_n;//return the x matrix
+}
+
+Mat<double>GetNext
