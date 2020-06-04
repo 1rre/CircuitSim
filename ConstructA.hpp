042190a19e27db5b794f541948a091ab3ff82e34
sc9318 <sc9318@ic.ac.uk>
2020-06-04 14:17:48 +0100

    file

--- ConstructA.hpp
+++ ConstructA.hpp
@@ -7,10 +7,7 @@
 using namespace std;
 using namespace arma;
 
-Mat<double> GetA()
-{
-;
-}
+
 Mat<double> MatrixG(Sim s) // vector of resistors
 {
   double N = s.nodes.size();
@@ -43,14 +40,14 @@ Mat<double> MatrixB(Sim s){ //Calculates Matrix B and stores the result in matri
 			M+=1;
 		}
 	}
-  double N = s.nodes.size();
+  double N = s.nodes.size()-1;
 	Mat<double> MatrixB(N,M,fill::zeros);
 	for(int i = 0; i < s.sources.size(); i++){
     if ((s.sources[i].cName=='V') || (s.sources[i].cName=='L')) {
-    	double pos = -1;//s.sources[i].pos->ID-1;
-			double neg = 0;//s.sources[i].neg->ID-1;
-			//cout<<"neg: "<<neg<<endl;
-			//cout<<"pos: "<<pos<<endl;
+    	double pos = s.sources[i].pos->ID-1;
+			double neg = s.sources[i].neg->ID-1;
+			cout<<"neg: "<<neg<<endl;
+			cout<<"pos: "<<pos<<endl;
 			if(pos>=0 && neg>=0)
 			{
 				MatrixB(i,pos)=1;
@@ -78,7 +75,7 @@ Mat<double> MatrixC(Sim s)
 
 Mat<double> MatrixD(Sim s)
 {
-	double M = 0;
+	double M = 1;
 	for (size_t i = 0; i < s.sources.size(); i++)
 		{
 		if ((s.sources[i].cName=='V') || (s.sources[i].cName=='L')) {
@@ -87,4 +84,38 @@ Mat<double> MatrixD(Sim s)
 	Mat<double> D(M,M,fill::zeros);
 	return D;
 }
+}
+
+Mat<double> GetA(Sim s)
+{
+double N = s.nodes.size()-1;
+double M = 0;
+for (size_t i = 0; i < s.sources.size(); i++) {
+	if ((s.sources[i].cName=='V') || (s.sources[i].cName=='L')) {
+		M+=1;
+	}
+}
+Mat<double> A((N+M),(N+M),fill::zeros);
+//cout<<A<<endl;
+Mat<double> G = MatrixG(s);
+//cout<<G<<endl;
+Mat<double> B = MatrixB(s);
+//cout<<B<<endl;
+Mat<double> C = MatrixC(s);
+//cout<<C<<endl;
+Mat<double> D = MatrixD(s);
+//cout<<D<<endl;
+//cout<<"N"<<N<<endl;
+//cout<<"M"<<M<<endl;
+A(span(0,N-1),span(0,N-1)) = G;
+//cout<<"G"<<endl;
+//cout<<A<<endl;
+A(span(0,N-1),span(N,N+M-1)) = B;
+//cout<<"B"<<endl;
+A(span(N,N+M-1),span(0,N-1)) = C;
+//cout<<"C"<<endl;
+A(span(N,N+M-1),span(N,N+M-1)) = D;
+//cout<<"D"<<endl;
+//cout<<A<<endl;
+return A;
 }
